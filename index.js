c2eeb7ed1bed9913d219a9c5437bb17d40bb594e
Daniel Ko <dankomong@gmail.com>
2019-02-25 00:03:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-function takeANumber(katzDeliLine, name) {
-  if (katzDeliLine.length === 0) {
-    katzDeliLine.push(name);
-    return `Welcome, ${name}. You are number 1 in line.`
+// function takeANumber(katzDeliLine, name) {
+//   if (katzDeliLine.length === 0) {
+//     katzDeliLine.push(name);
+//     return `Welcome, ${name}. You are number 1 in line.`
+//   }
+//   else if (katzDeliLine.length > 0) {
+//     katzDeliLine.push(name);
+//     return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+//   }
+// }
+
+var amount = 0;
+
+function takeANumber(line) {
+      amount++;
+      line.push(amount);
+      return `You are number ${amount}.`
   }
-  else if (katzDeliLine.length > 0) {
-    katzDeliLine.push(name);
-    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
-  }
+  
 }
+
+takeANumber(line) -> 1 
+takeANumber(line) -> 2 
+nowServing(line) -> 1 
+nowServing(line) -> 2 
+takeANumber(line) -> 3
 
 function nowServing(katsDeliLine) {
   if (katsDeliLine.length === 0) {
